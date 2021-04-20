2c6d6e7c2c818c74d8381ac77bf965a3e8abc6dd
somura6891 <somx@yandex.ru>
2021-04-20 13:39:54 +0300

    Написал текст

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+        String mytext = "GOD";
+
+        switch (mytext) {
+
+            case "GOD1":
+                System.out.println("GOD1");
+            case "GOD":
+                System.out.println("GOD");
+            case "GOD2":
+                System.out.println("GOD2");
+            case "GOD3":
+                System.out.println("GOD3");
+
+        }
+
+    }
+}
